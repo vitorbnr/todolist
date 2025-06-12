42c67f6fc5a1a1ede044d520210a9c3df9352a3d
vitorbnr <vitorbernardotech@gmail.com>
2025-06-11 22:08:15 -0300

    feat: criacao de endpoints

---
+++ src/main/java/com/vitorbnr/todolist/controller/TodoController.java
@@ -0,0 +1,37 @@
+package com.vitorbnr.todolist.controller;
+
+import com.vitorbnr.todolist.entity.Todo;
+import com.vitorbnr.todolist.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping()
+    List<Todo> create(@RequestBody Todo todo) {
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> list() {
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable ("id") Long id) {
+        return todoService.delete(id);
+    }
+}
