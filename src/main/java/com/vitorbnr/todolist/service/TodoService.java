0cb78e38190ed1c7790ce78bbc6c551183061747
vitorbnr <vitorbernardotech@gmail.com>
2025-06-11 21:55:02 -0300

    feat: implementacao de servico

---
+++ src/main/java/com/vitorbnr/todolist/service/TodoService.java
@@ -0,0 +1,39 @@
+package com.vitorbnr.todolist.service;
+
+import com.vitorbnr.todolist.entity.Todo;
+import com.vitorbnr.todolist.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by(Sort.Direction.DESC, "prioridade")
+                .and(Sort.by(Sort.Direction.ASC, "id"));
+
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
