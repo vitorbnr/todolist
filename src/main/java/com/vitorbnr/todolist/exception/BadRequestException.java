9505aadd5aeba3b7c5cc60a692cd45f7e901c351
vitorbnr <vitorbernardotech@gmail.com>
2025-06-12 23:09:51 -0300

    feat: criacao de excecoes

---
+++ src/main/java/com/vitorbnr/todolist/exception/BadRequestException.java
@@ -0,0 +1,9 @@
+package com.vitorbnr.todolist.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+}
